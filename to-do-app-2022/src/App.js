30a0129b5ec6f22f36e758cfe6540bd6270c1c02
Raymond Amparo <ray.amparo1995@gmail.com>
2022-12-13 14:38:33 -0800

    initial create react app

---
+++ to-do-app-2022/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Router from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        {/* <Route path='/' component={}/> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
