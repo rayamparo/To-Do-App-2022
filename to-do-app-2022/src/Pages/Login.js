58c63ed5eec6b1a3f1d4febeae1b34efc28f5e9f
Raymond Amparo <ray.amparo1995@gmail.com>
2023-01-25 11:27:28 -0800

    finished layout for login page

--- to-do-app-2022/src/Pages/Login.js
+++ to-do-app-2022/src/Pages/Login.js
@@ -1,8 +1,11 @@
-import React, {Fragment} from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Material UI
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const Login = () => {
 
@@ -20,7 +23,42 @@ const Login = () => {
         fontSize: '20px',
         fontWeight: '500',
         float: 'left',
-        margin: '20px'
+        marginTop: '30px',
+        marginLeft: '54px'
+      }
+
+      const textFieldWidth = {
+        width: '350px',
+        // margin: '30px',
+        backgroundColor: '#EAE9EC',
+        marginTop: '-10px'
+      }
+
+      const par = {
+        fontFamily: 'Montserrat, sans-serif',
+        float: 'left',
+        marginLeft: '54px',
+        color: '#737274',
+        marginTop: '20px'
+      }
+
+      const par2= {
+        fontFamily: 'Montserrat, sans-serif',
+        float: 'left',
+        marginLeft: '54px',
+        color: '#737274',
+        marginTop: '40px'
+      }
+
+      const btn = {
+        fontFamily: 'Montserrat, sans-serif',
+        marginTop: '40px',
+        backgroundColor: '#02F9BC',
+        color: 'black'
+      }
+
+      const linkStyle = {
+        color: '#02F9BC'
       }
       
 
@@ -42,10 +80,35 @@ const Login = () => {
                             boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.7), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
                         }}
                     >
-                        
-                        <h6 style={loginH6}>
-                            Login
-                        </h6>
+                    <Grid container spaceing={3}>
+                        <Grid item xs={12}>
+                            <h6 style={loginH6}>
+                                Login
+                            </h6>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <p style={par}>
+                                Email
+                            </p>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField style={textFieldWidth} variant="outlined"/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <p style={par2}>
+                                Password
+                            </p>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField style={textFieldWidth} variant="outlined"/>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button style={btn} size="large">Login</Button>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <p>Don't have an account? <Link to='/signup' style={linkStyle}>Create one</Link></p>
+                        </Grid>
+                    </Grid>
                     </Box>
                 </Grid>
             </Grid>
